2395f50423abed591f74e32b0c8496205dee2820
arshxb <arshsasan@gmail.com>
2021-12-14 03:24:03 -0600

    css done

--- src/routes/about-us.svelte
+++ src/routes/about-us.svelte
@@ -1,2 +1,6 @@
+<script>
+    import "../app.css";
+</script>
+
 <h1> About us</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,12 +9,46 @@
     </head>
 </svelte:head>
 
-<h1>{title}</h1>
+<style>
+    .todos{
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add todo"/>
-</form>
+    .new{
+        margin: 0 0 0.5rem 0;
+    }
 
-<TodoItem></TodoItem>
-<TodoItem></TodoItem>
-<TodoItem></TodoItem>
+    .new input{
+        font-size: 28px;
+        width: 100%;
+        padding: 00.5em 1em 0.3em 1em;
+        box-sizing: border-box;
+        background: rgba(255, 255, 255, 0.05);
+        border-radius: 8px;
+        text-align: center;
+    }
+
+    .todos :global(input){
+        border: 1px solid transparent;
+    }
+
+    .todos :global(input:focus-visible){
+        box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 1);
+        border: 1px solid #ff3e00 !important;
+        outline: none;
+    }
+</style>
+
+<div class="todos">    
+    <h1>{title}</h1>
+    
+    <form action="" method="" class="new">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add todo"/>
+    </form>
+    
+    <TodoItem></TodoItem>
+    <TodoItem></TodoItem>
+    <TodoItem></TodoItem>
+</div>
