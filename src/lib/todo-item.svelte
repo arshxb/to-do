1c18df86397080c24bedd8316e5942a9e9380028
arshxb <arshsasan@gmail.com>
2021-12-14 00:57:48 -0600

    Dev html struc Fixes #3

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,16 @@
+<div>
+
+    <form action="" method="">
+        <input type="hidden" name="done" value=""/>
+        <button aria-label="Mark done/not done">Done/not-done</button>
+    </form>
+
+    <form action="" method="" >
+        <input type="text"/>
+        <button aria-label="save todo">save button</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete todo">Delete</button>
+    </form>
+</div>
